d6665d5104a959cda650a2a4e76175479bffdd54
sujalsaini3304 <sujalsaini3304@gmail.com>
2026-03-04 23:29:37 +0530

    viewing image on click done successfully

--- src/components/created_ui/GridBox.jsx
+++ src/components/created_ui/GridBox.jsx
@@ -1,31 +1,51 @@
-const GridBox = (
-    { images }
-) => {
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+// import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+const GridBox = ({ images }) => {
+  const [index, setIndex] = useState(-1);
+
   return (
     <div className="p-4">
-      <div
-        className="
-          grid 
-          grid-cols-2 
-          sm:grid-cols-3 
-          md:grid-cols-4 
-          lg:grid-cols-5 
-          gap-4
-        "
-      >
-        {images.map((img, index) => (
+      {/* GRID */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {images.map((img, i) => (
           <div
-            key={index}
-            className="cursor-pointer overflow-hidden rounded-xl shadow-md"
+            key={i}
+            onClick={() => setIndex(i)}
+            className="cursor-pointer rounded-xl overflow-hidden shadow-md"
           >
             <img
               src={img}
-              alt={`img-${index}`}
-              className="w-full h-40 object-cover hover:scale-105 transition-transform duration-300"
+              className="w-full h-40 object-cover hover:scale-105 transition"
             />
           </div>
         ))}
       </div>
+
+      {/* LIGHTBOX */}
+      <Lightbox
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        slides={images.map((img) => ({ src: img }))}
+        plugins={[Zoom,
+          //  Thumbnails
+        ]}
+        //         thumbnails={{
+        //   position: "bottom",
+        //   width: 90,
+        //   height: 70,
+        //   border: 2,
+        //   imageFit: "cover",
+        // }}
+        zoom={{
+          maxZoomPixelRatio: 3,
+        }}
+      />
     </div>
   );
 };
